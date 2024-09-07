585b8627f0a4c11aed4b3e65c71b8e995adae137
Akachukwu Chukwuemerie David <63329343+Imdavyking@users.noreply.github.com>
2024-09-07 18:49:43 +0100

    writing test for smart contract

--- tests/match-solana-contract.ts
+++ tests/match-solana-contract.ts
@@ -1,16 +1,16 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { MatchSolanaContract } from "../target/types/match_solana_contract";
+import { Marketplace } from "../target/types/marketplace";
 
 describe("match-solana-contract", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.MatchSolanaContract as Program<MatchSolanaContract>;
+  const program = anchor.workspace.Marketplace as Program<Marketplace>;
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods.initializeCounters().rpc();
     console.log("Your transaction signature", tx);
   });
 });

151985c6d597ff70ad40e09a65d6bb73cff43b31
Akachukwu Chukwuemerie David <63329343+Imdavyking@users.noreply.github.com>
2024-09-08 19:16:48 +0100

    added

--- programs/match-solana-contract/src/lib.rs
+++ programs/match-solana-contract/src/lib.rs
@@ -6,7 +6,7 @@ use anchor_lang::prelude::*;
 
 declare_id!("EPDpaEoRGQbZHBG1wJkd4Vae44UPmTLMmDreLcjrkfAg");
 use crate::{constants::*, events::*, states::*, errors::*};
-use solana_program::pubkey;
+// use solana_program::pubkey;
 use std::mem::size_of;
 
 // const ADMIN_PUBKEY: Pubkey = pubkey!("7iT5H86QPoNFjGt1X2cMEJot4mr5Ns4uzhLN3GJKQ5kk");
